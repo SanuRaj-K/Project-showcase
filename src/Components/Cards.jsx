f3797ab86c8a176532286350478037f25eca5952
SanuRaj-K <sanusrk29@gmail.com>
2024-05-27 16:11:25 +0530

    added projects

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,66 +1,58 @@
 import React from "react";
-import blogsupLogo from "../Blogsup-logo-jpg.jpg";
-import petShop from "../Components/petshop.png";
+import { data } from "../Data";
 function Cards() {
+  console.log(data);
   return (
     <div>
       <section className="text-gray-400   body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col">
-            <div className="h-1  rounded overflow-hidden">HAI</div>
-          </div>
+          <div className="flex flex-col"></div>
           <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img
-                  alt="content"
-                  className="object-cover object-center h-full "
-                  src="https://www.shutterstock.com/image-vector/amazon-initial-logo-isolated-white-600nw-2272856739.jpg"
+            {data?.map((item) => (
+              <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+                <div className="rounded-lg h-64 overflow-hidden">
+                  <img
+                    alt={item.title}
+                    className="object-cover object-center h-full "
+                    src={item.image}
+                  />
+                </div>
+                <h2 className="text-xl title-font text-black font-semibold my-5">
+                  {item.title}
+                </h2>
+                <p className="text-base sm:min-h-[70px] leading-relaxed mt-2">
+                  {item.description}
+                </p>
+                <div className=" my-2">
+                  <button
+                    className="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    data-ripple-light="true"
+                  >
+                    <a href={item.frontend}>Frontend</a>
+                  </button>
+                  <button
+                    className="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    data-ripple-light="true"
+                  >
+                    <a href={item.backend}>Backend</a>
+                  </button>
+                  <button
+                    className="middle  mt-2  none center mr-4 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    data-ripple-light="true"
+                  >
+                    <a href={item.live}>Live</a>
+                  </button>
+                </div>
+                <link
+                  rel="stylesheet"
+                  href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
                 />
-              </div>
-              <h2 className="text-xl title-font text-black font-semibold my-5">
-                Amazon-clone
-              </h2>
-              <p className="text-base sm:min-h-[70px] leading-relaxed mt-2">
-                Created an Amazon clone website involves replicating the core
-                functionalities and features of the original Amazon platform.
-                This includes creating user accounts, browsing products, adding
-                items to a shopping cart,and managing orders.
-              </p>
-              <div className=" my-2">
-                <button
-                  className="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  data-ripple-light="true"
-                >
-                  <a href="https://github.com/SanuRaj-K/amazon-clone-frontEnd">
-                    Frontend
-                  </a>
-                </button>
-                <button
-                  className="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  data-ripple-light="true"
-                >
-                  <a href="https://github.com/SanuRaj-K/amazon-clone-backend">
-                    Backend
-                  </a>
-                </button>
-                <button
-                  className="middle  mt-2  none center mr-4 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  data-ripple-light="true"
-                >
-                  <a href="https://amazon-clone-front-end-sage.vercel.app/">
-                    Live
-                  </a>
-                </button>
-              </div>
-              <link
-                rel="stylesheet"
-                href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
-              />
 
-              <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
-            </div>
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+                <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+              </div>
+            ))}
+
+            {/* <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
               <div className="rounded-lg h-64 overflow-hidden">
                 <img
                   alt="content"
@@ -99,8 +91,8 @@ function Cards() {
                   <a href="https://blogsup.shop/">Live</a>
                 </button>
               </div>
-            </div>
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            </div> */}
+            {/* <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
               <div className="rounded-lg h-64 overflow-hidden">
                 <img
                   alt="content"
@@ -133,7 +125,7 @@ function Cards() {
                   <a href="https://github.com/SanuRaj-K/Node">Backend</a>
                 </button>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    title: "amazon-clone",
+    description: `Created an Amazon clone website involves replicating the core
+    functionalities and features of the original Amazon platform.
+    This includes creating user accounts, browsing products, adding
+    items to a shopping cart,and managing orders.`,
+    frontend: "https://github.com/SanuRaj-K/amazon-clone-frontEnd",
+    backend: "https://github.com/SanuRaj-K/amazon-clone-backend",
+    live: "https://amazon-clone-front-end-sage.vercel.app/",
+    image:
+      "https://www.shutterstock.com/image-vector/amazon-initial-logo-isolated-white-600nw-2272856739.jpg",
+  },
+  {
+    title: "Blogs-up",
+    description: `   Blogsup is a Social media platform. Users can Read blogs and
+    interact with that blogs by giving like and comment.`,
+    frontend: "https://github.com/SanuRaj-K/blog-s_up_frontEnd",
+    backend: "https://github.com/SanuRaj-K/blog-s_up-backend",
+    live: "https://blogsup.shop/",
+    image:
+      "https://res.cloudinary.com/dgoermgtp/image/upload/v1716805509/Projects/Blogsup-logo-jpg_gsm4m7.jpg",
+  },
+  {
+    title: " PetFood Shop",
+    description: `PetFood Shop is a E-commerce platform. Users can View and buy
+    pet food products. Currently this server is not live`,
+    frontend: "https://github.com/SanuRaj-K/PetFoodShop",
+    backend: "https://github.com/SanuRaj-K/Node",
+    image:
+      "https://res.cloudinary.com/dgoermgtp/image/upload/v1716805567/Projects/petshop_x8gucf.png",
+  },
+  {
+    title: "Todo-Firebase",
+    description: `Created an todo app using firebase authentication and storage. Its a  todo app. Users can easly regiter and use this todo app by using google authentication`,
+    frontend: "https://github.com/SanuRaj-K/firebase-todo",
+    backend: "",
+    live: "firebase-todo-delta.vercel.app",
+    image:'https://res.cloudinary.com/dgoermgtp/image/upload/v1716806289/Projects/todo-logo_twpge2.jpg'
+  },
+
+];
